56462f315ae929bd6c690b393534f224bfac9c86
Isa Moura <isamariamoura03@gmail.com>
2024-06-14 01:43:47 -0300

    Alteração de modelo por marca mês de Abril

---
+++ tratamentoAbr.py
@@ -0,0 +1,26 @@
+month_movement = pd.read_excel('/content/Abril.xlsx')
+
+replace_models = {
+  'samsunga21s': 'samsung',
+  'sansung': 'samsung',
+  's22ultra': 'samsung',
+  'moto': 'motorola',
+}
+
+month_movement = month_movement.drop(7)
+month_movement = month_movement.dropna()
+
+def replace_phone_model_name(array):
+  phone_model_name = []
+
+  for x in array:
+    first_word = str(x).split(' ', 1)[0]
+    phone_model = first_word.lower()
+
+    if(phone_model in replace_models.keys()):
+      phone_model = replace_models[phone_model]
+
+    phone_model_name.append(phone_model)
+
+  return phone_model_name
+#
